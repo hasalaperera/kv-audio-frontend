10487862320a6b4e9e46df6f430e2a26619435ff
hasalaperera <hasala294@gmail.com>
2025-03-03 11:18:34 +0530

    add react icons 🎉

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,37 @@
-import './App.css'
-import ProductCard from './components/productCard'
+import "./App.css";
+import ProductCard from "./components/productCard";
+import { BsGraphDown } from "react-icons/bs";
+import { FaRegBookmark } from "react-icons/fa";
+import { MdOutlineSpeaker } from "react-icons/md";
+import { FaRegUser } from "react-icons/fa";
+
+
 
 function App() {
-  
-
   return (
-    <div className=''>
-      
-      <h1>Products</h1>
+    <div className="w-full h-screen flex">
+      <div className="w-[400px] h-full bg-green-200">
+        <button className="w-full h-[40px] text-[25px] font-bold flex justify-center items-center">
+          <BsGraphDown/>
+          Dashbord
+        </button>
+        <button className="w-full h-[40px] text-[25px] font-bold flex justify-center items-center">
+          <FaRegBookmark/>
+          Bookings
+        </button>
+        <button className="w-full h-[40px] text-[25px] font-bold flex justify-center items-center">
+          <MdOutlineSpeaker/>
+          Items
+          </button>
+        <button className="w-full h-[40px] text-[25px] font-bold flex justify-center items-center">
+          <FaRegUser/>
+          Users
+          </button>
+      </div>
 
-
+      <div className="w-full bg-red-900"></div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
