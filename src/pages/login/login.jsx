5ff2f5869ba9d8c34751c615d0dfd5f07a18fc03
hasalaperera <hasala294@gmail.com>
2025-03-14 00:37:08 +0530

    use navigate hook

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -2,10 +2,12 @@ import axios from "axios";
 import "./login.css";
 import { useState } from "react";
 import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 export default function LoginPage  () {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const navigate = useNavigate();
 
     function handleOnSubmit(e) {
         e.preventDefault();
@@ -19,6 +21,15 @@ export default function LoginPage  () {
         ).then((res) => {
             console.log(res);
             toast.success("Login Success");
+
+            const user = res.data.user
+
+            if(user.role === "admin") {
+                navigate("/admin");
+            }else {
+                navigate("/");
+            }
+
         }).catch((err) => {
             console.log(err);
             toast.error("err.response.data.error");
